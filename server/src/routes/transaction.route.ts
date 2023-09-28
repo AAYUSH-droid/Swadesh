82669adfa0ba4ef8cf8197a55f5e8e5f21213404
aayush <sharma31aayush@gmail.com>
2023-09-28 23:26:17 +0530

    init: server init

--- server/src/routes/transaction.route.ts
+++ server/src/routes/transaction.route.ts
@@ -1,19 +1,20 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
   createCreditTransactionHandler,
   createDebitTransactionHandler,
   createTransferTransactionHandler,
   deleteTransactionHandler,
   getAllTransactionByUserIdHandler,
-} from '../controllers/transaction.controller';
+} from "../controllers/transaction.controller";
 
 const router = Router();
 
-router.post('/credit', createCreditTransactionHandler);
-router.post('/debit', createDebitTransactionHandler);
-router.post('/transfer', createTransferTransactionHandler);
+//routes
+router.post("/credit", createCreditTransactionHandler);
+router.post("/debit", createDebitTransactionHandler);
+router.post("/transfer", createTransferTransactionHandler);
 
-router.get('/', getAllTransactionByUserIdHandler);
-router.delete('/', deleteTransactionHandler);
+router.get("/", getAllTransactionByUserIdHandler);
+router.delete("/", deleteTransactionHandler);
 
 export default router;
